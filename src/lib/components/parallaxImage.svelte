fbd3e753e82abe5941a518b16256cff85a5fe83e
MCT32 <32090502+MCT32@users.noreply.github.com>
2023-11-02 21:57:49 +1100

    Added parallax background to homepage

---
+++ src/lib/components/parallaxImage.svelte
@@ -0,0 +1,17 @@
+<script>
+    /** @type {string} */
+    export let src;
+</script>
+
+<div class="parallax" style="background-image: url({src});">
+    <slot />
+</div>
+
+<style>
+    .parallax {
+        background-attachment: fixed;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,16 @@
 <script>
-    import Wip from "$lib/components/wip.svelte";
+    import ParallaxImage from "$lib/components/parallaxImage.svelte";
 </script>
 
-<Wip />
+<ParallaxImage src="background.png">
+    <div class="px-20 py-40">
+        <h1 class="text-white font-semibold text-9xl">
+            Hi,
+            <br>
+            I'm <b>MCT32</b>
+        </h1>
+        <p class="text-white text-xl">
+            I'm a hobby developer that never finishes my projects. ;)
+        </p>
+    </div>
+</ParallaxImage>
