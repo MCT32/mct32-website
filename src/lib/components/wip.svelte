e0e6df71e7ec2a5114a76ce8bc2eab387e452a2c
MCT32 <32090502+MCT32@users.noreply.github.com>
2023-11-02 18:33:22 +1100

    Made icon bigger

--- src/lib/components/wip.svelte
+++ src/lib/components/wip.svelte
@@ -1,5 +1,5 @@
 <div class="text-center py-10">
     <h2 class="text-xl font-bold text-gray-800">I haven't made this yet! Come back later.</h2>
     <br>
-    <img src="obstacle.png" alt="WIP icon" class="w-60 mx-auto">
+    <img src="obstacle.png" alt="WIP icon" class="w-80 mx-auto">
 </div>
