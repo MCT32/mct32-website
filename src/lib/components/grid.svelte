b773bd6b91ca1271bcf923cb7e0e7117dd327f2c
MCT32 <32090502+MCT32@users.noreply.github.com>
2023-11-03 19:21:23 +1100

    Removed images from projects

--- src/lib/components/grid.svelte
+++ src/lib/components/grid.svelte
@@ -6,8 +6,6 @@
         {
             title: "VelocityEngine",
             description: "Shitty, unfinished Wolfenstein clone",
-            src: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.6mvzo-QSSaASiqJG2PnlGAHaEo%26pid%3DApi&f=1&ipt=a863ca19e68cff54bf67889dd01dd10815458fe118d14a70dcfdb7c3d9017323&ipo=images",
-            alt: "Wolfenstein screenshot",
             href: "https://github.com/MCT32/VelocityEngine",
             tags: [
                 "Rust",
@@ -16,8 +14,6 @@
         {
             title: "DOOM Melting Transition",
             description: "Recreationn of the DOOM level transition in Python. Exports to GIF.",
-            src: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.JhqeWkbt9YhsDCtk_tkiRQHaEo%26pid%3DApi&f=1&ipt=5df1fe31c995218e9d51c8a063986d03c97c6b71672a40f0f30d66592791aa8d&ipo=images",
-            alt: "Doom art",
             href: "https://github.com/MCT32/Doom-Melting-Transition",
             tags: [
                 "Python",
@@ -26,8 +22,6 @@
         {
             title: "Discord Soundboard",
             description: "VC soundboard bot that can be controlled from outside the VC. Made before soundboards were officially added to Discord.",
-            src: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-world.net%2Fwp-content%2Fuploads%2F2020%2F12%2FDiscord-Logo.png&f=1&nofb=1&ipt=d61746e744d6222360ed8ed7661396042ce6eefdaacc44140962d22d9dc3ca42&ipo=images",
-            alt: "Discord screenshot",
             href: "https://github.com/MCT32/discord-soundboard",
             tags: [
                 "Javascript",
@@ -37,8 +31,6 @@
         {
             title: "Zombie Shooter",
             description: "2D zombie shooter survival game.",
-            src: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.yHHLoIu8WGhvUVvgUwMYDwHaHa%26pid%3DApi&f=1&ipt=0ce2b0e46930b659078f7083e24cd2bacc29b35afa47ac859f389103ffe4cef2&ipo=images",
-            alt: "Zombie head",
             href: "https://github.com/MCT32/ZombieShooter",
             tags: [
                 "Python",
@@ -85,13 +77,13 @@
         {/each}
     </div>
     <div class="grid grid-cols-3 gap-4">
-        {#each projects as {title, description, src, alt, href, tags}}
+        {#each projects as {title, description, href, tags}}
             {#if enabledTags.length > 0}
                 {#if tags.filter(element => enabledTags.includes(element)).length > 0}
-                    <GridItem {description} {title} {src} {alt} {href} />
+                    <GridItem {description} {title} {href} />
                 {/if}
             {:else}
-                <GridItem {description} {title} {src} {alt} {href} />
+                <GridItem {description} {title} {href} />
             {/if}
         {/each}
     </div>
--- src/lib/components/gridItem.svelte
+++ src/lib/components/gridItem.svelte
@@ -4,17 +4,10 @@
     /** @type {string} */
     export let description;
     /** @type {string} */
-    export let src;
-    /** @type {string} */
-    export let alt;
-    /** @type {string} */
     export let href;
 </script>
 
-<a class="border-gray-500 border rounded-lg p-3 inline-flex" href={href}>
-    <img class="aspect-square w-60 rounded" {src} {alt}>
-    <div class="pl-5">
-        <h3 class="font-semibold text-lg">{title}</h3>
-        <p>{description}</p>
-    </div>
+<a class="border-gray-500 border rounded-lg p-3" href={href}>
+    <h3 class="font-semibold text-lg">{title}</h3>
+    <p>{description}</p>
 </a>
