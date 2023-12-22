8819a5c8ca3cbb7dc49a38b80971a44c2aca10db
MCT32 <32090502+MCT32@users.noreply.github.com>
2023-12-23 02:57:51 +1100

    added lorem ipsum

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script>
     import ParallaxImage from "$lib/components/parallaxImage.svelte";
-    import Grid from "$lib/components/grid.svelte";
 </script>
 
 <ParallaxImage src="background.png">
@@ -15,4 +14,11 @@
         </p>
     </div>
 </ParallaxImage>
+
+<h2 class="font-semibold text-5xl m-5 text-center">Lorem Ipsum</h2>
+<p class="text-lg">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam neque eros, sollicitudin id mi ac, aliquet facilisis nisl. Sed ut enim id quam semper interdum non et lorem. Proin auctor nisl eget vulputate interdum. Praesent vehicula odio a arcu finibus faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce in mi vel leo dictum feugiat. Curabitur fermentum felis sit amet lectus ornare luctus. Fusce pellentesque rhoncus pretium. Cras nec lobortis lacus, nec vulputate sem. Duis augue turpis, laoreet nec velit vel, vulputate mattis tellus. Pellentesque at tellus rutrum, gravida lectus eleifend, eleifend ante. Duis lectus augue, dapibus in mi in, feugiat imperdiet ante. Vestibulum placerat lorem id nisi sodales, ac luctus elit aliquet.
+
+Aenean at condimentum magna. Aenean eleifend nulla id justo egestas, vitae tristique risus sagittis. Sed non sapien bibendum, imperdiet nibh non, pharetra arcu. In id arcu feugiat, convallis magna vitae, aliquam dui. Aenean lacinia ut leo vitae suscipit. Nunc pharetra massa non scelerisque semper. Fusce vel accumsan ante. Proin eu lobortis arcu. Aliquam venenatis varius turpis non rhoncus. In eu congue nisl. Morbi elit odio, efficitur eu finibus ac, maximus ac purus. Donec quis molestie nibh, sit amet eleifend quam. Pellentesque volutpat tincidunt dolor eu semper. Curabitur maximus ipsum bibendum nunc vestibulum, eget sollicitudin risus efficitur. 
+</p>
 
